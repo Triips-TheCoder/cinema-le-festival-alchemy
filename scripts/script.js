d6f28181ca65fe0cc44dc7a08c2c499610c3c89a
TheoSaminadin <theo.saminadin@mmibordeaux.com>
2020-10-22 22:15:18 +0200

    ajout du à propos

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,31 @@
-
 var positionbase = window.scrollY;
 
 window.addEventListener("scroll", function() {
-  var scollY = window.scrollY;
-  if(scrollY < positionbase) {
-    document.getElementById("myHeader").style.top = "0px";
-  }else { 
-    document.getElementById("myHeader").style.top = "-120px";
-  }
-  positionbase = scrollY;
+    var scollY = window.scrollY;
+
+    if (scrollY < positionbase) {
+        document.getElementById("myHeader").style.top = "0px";
+    } else {
+        document.getElementById("myHeader").style.top = "-120px";
+        if (scrollY < 100) {
+            document.getElementById("myHeader").style.top = "0px";
+        }
+    }
+    positionbase = scrollY;
 });
 
 const filmButtons = document.querySelectorAll('.film_nav');
 
-filmButtons.forEach(function (button, index) {
+filmButtons.forEach(function(button, index) {
     button.addEventListener('click', function() {
         activateFilm(index)
     });
 });
 
-function activateFilm (wantedFilmIndex) {
+function activateFilm(wantedFilmIndex) {
     const films = document.querySelectorAll('.description_container');
-    
-    films.forEach(function (film, index) {
+
+    films.forEach(function(film, index) {
         if (index === wantedFilmIndex) {
             film.classList.add('is-active');
         } else {
@@ -30,7 +33,7 @@ function activateFilm (wantedFilmIndex) {
         }
     });
 
-    filmButtons.forEach(function (button, index) {
+    filmButtons.forEach(function(button, index) {
         if (index === wantedFilmIndex) {
             button.classList.add('is-active');
         } else {
